ad4a2cff1e08aeecb53c518dab3899a2e6efb27b
lewee <leween.m33@gmail.com>
2023-04-15 12:06:07 +0200

    optimize quest_&_answ_randomizer.py + randomize answers

---
+++ quest_&_answ_randomizer.py
@@ -0,0 +1,19 @@
+from random import randint, shuffle
+from xlsx_extractor import Extractor
+
+
+class Randomizer:
+    def __init__(self, xlsx_file_name: str, list_nbr_questions: list):
+        work_dict = Extractor(xlsx_file_name).work_dict
+        self.dico_quest_ans = {}
+        print(work_dict)
+        for sheet in work_dict:
+            list_questions = list(work_dict[sheet])
+            shuffle(list_questions)
+            self.dico_quest_ans[sheet] = {}
+            for i in range(list_nbr_questions[0]):
+                list_answers = list(work_dict[sheet][list_questions[0]])
+                shuffle(list_answers)
+                self.dico_quest_ans[sheet][list_questions[0]] = list_answers
+                list_questions.pop(0)
+            list_nbr_questions.pop(0)
