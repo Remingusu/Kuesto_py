ea6a9df19b49f5f2585c6a2f19a4ac8c1ce5fa04
lewee <leween.m33@gmail.com>
2023-04-15 12:05:51 +0200

    optimize quest_&_answ_randomizer.py + randomize answers

--- file_generator.py
+++ file_generator.py
@@ -1,23 +1,23 @@
 from docx import Document
 from docx.shared import Pt, Inches
-from questions_randomizer import Randomizer
+from questansw_randomizer import Randomizer
 
 
 class Generator:
     def __init__(self, xlsx_file_name: str, list_nbr_question: list, nbr_copies: int, qcm_file_prefix: str):
-        quest_and_answer = Randomizer('exemple', [2, 3]).dico_quest_ans
-        print(quest_and_answer)
         for i in range(nbr_copies):
+            quest_and_answer = Randomizer('exemple', [2, 3]).dico_quest_ans
+            print(quest_and_answer)
             file = Document()
             for sheet in quest_and_answer:
                 quest_and_answer_ws = quest_and_answer[sheet]
-                for question in quest_and_answer_ws.values():
+                for question in quest_and_answer_ws.keys():
                     quest_and_answer_wsiq = quest_and_answer_ws[question]
                     pg = file.add_paragraph(style='List Number').add_run(question)
                     pg.font.size = Pt(12)
                     pg.font.name = 'Calibri Light'
                     for i1 in range(len(quest_and_answer_wsiq)):
-                        file.add_paragraph()
+                        pg = file.add_paragraph()
                         pg.paragraph_format.left_indent = Inches(0.24)
                         # interligne
                         pg_run = pg.add_run(f"□ {chr(65 + i1)}. {quest_and_answer_wsiq[i1]}")
